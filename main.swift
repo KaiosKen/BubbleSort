81c67691fa624322983d1a99b83363a01fd0a9fa
Robel Abraham <robel.abraham@student.allenisd.org>
2022-10-08 19:10:28 -0500

    Made the bubble sorting algorithm much more optimal

--- main.swift
+++ main.swift
@@ -9,11 +9,9 @@ var randomWords : [String] = []
 // Read the contents of the specified file
 do {
     //This do-catch program puts a txt file into an array and seperates it into multiple values
-    let data = try String(contentsOfFile:"random.txt", encoding: String.Encoding.utf8)
+    let data = try String(contentsOfFile:"random-10e3b.txt", encoding: String.Encoding.utf8)
     randomWords = data.components(separatedBy: "\n")
     randomWords.removeLast()
-    print(randomWords[0])
-
 } catch let err as NSError {
     // do something with Error
     print(err)
@@ -22,6 +20,7 @@ do {
 
 var sequence = 0
 var wordCount = randomWords.count - 1
+print(wordCount)
 func Sorter(words : Array<String>, arrayCount: Int, wordCount: Int) -> Array<String> {
     var sortedWords = words
     var sequence2 = 0
@@ -35,50 +34,47 @@ func Sorter(words : Array<String>, arrayCount: Int, wordCount: Int) -> Array<Str
                 orderCheck += 1
             }
         }
-        if orderCheck == wordCount
+        if orderCheck == wordCount-1
         {
             return true
+    //        print(words)
         } else {
+         //   print(orderCheck)
+  //          print(wordCount)
             return false
         }
 
     }
+    var y = 0
     while true {
         sequence2 = sequence + 1
-        var word1 : UInt8 = 1
-        word1 = (sortedWords[sequence].first!).asciiValue!
-
-        if word1 < 97 && word1 > 64 {
-            word1 += 32
-        }
-        var word2 : UInt8 = 1
-        word2 = (sortedWords[(sequence2)].first!).asciiValue!
-
-        if word2 < 97 && word2 > 64 {
-            word2 += 32
-        }
+        let word1 = sortedWords[sequence]
+        let word2 = sortedWords[sequence2]
         if word1 > word2 {
             sortedWords.swapAt(sequence, sequence2)
             sequence += 1
         } else {
             sequence += 1
         }
-        print(word1)
-        print(word2)
-        print(sequence)
         if sequence == wordCount{
             sequence = 0
             sequence2 = 0
-            print(sortedWords)
+            
         }
-        if checkSorted(words:sortedWords)
-        {
-            break
+        
+        if y >= 100 {
+            y = 0
+            if checkSorted(words:sortedWords)
+            {
+                break
+            }
         }
-
-
+        y += 1
 
     }
     return sortedWords
 }
-print(Sorter(words: randomWords, arrayCount:sequence, wordCount:wordCount))  
+if wordCount > 1 {
+   
+    print(Sorter(words: randomWords, arrayCount:sequence, wordCount:wordCount)) 
+} 
